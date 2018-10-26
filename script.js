450af4b8838e21af7788000ab078ec4c2c1d00ea
arvisakhavi <33436705+arvisakhavi@users.noreply.github.com>
2018-10-26 12:32:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function search (){
+    var artist = document.getElementById("artists").value;
+    var limit = document.getElementById("limit").value;
+    $.post('http://itunes.apple.com/search?limit=' + limit +'&term='+ artist, page);
+}
+
+function page(result){
+    console.log(JSON.parse(result));
+
+    var json = JSON.parse(result);
+    var songs = json.results;
+
+    $("#tableOne").find("td").remove();
+
+    for(var i = 0; i<songs.length;i++){
+        $("#tableOne").append("<tr id = 'row'>" +
+            "<td>" + songs[i].artistName + "</td>" +
+            "<td>" + songs[i].trackName + "</td>" +
+            "<td> <img src = " +songs[i].artworkUrl30+ "> </td>"+
+            "<td>" + songs[i].collectionName + "</td>" +
+            "<td><audio controls><source src='" + songs[i].previewUrl + "' type='audio/mpeg'></audio></td></tr>")
+
+    }
+    $("#tableOne").css("display","inline-block");
+
+}
